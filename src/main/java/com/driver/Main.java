82b458bac99ff16cfc2ba0380886152a865ad58e
Jawad TP <jawadtp248@gmail.com>
2022-10-15 22:24:05 +0530

    update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,16 @@
 package com.driver;
 
 public class Main {
+    public  static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public String meth(){
+            return "Invoking method from class B";
+        }
+    }
     public static void main(String[] args) {
         B a= new B();
         System.out.println(a.meth());
